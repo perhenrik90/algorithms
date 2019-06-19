770780869ef3b37e58e654cfb4b7952f620d906f
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2019-06-19 10:33:59 +0200

    har en fungernede kø

--- p1a2/Deque.java
+++ p1a2/Deque.java
@@ -1,37 +1,56 @@
-import java.util.*;
+import java.util.Iterator;
 
 
 
 
-public class Deque<Item> {
+public class Deque<Item> implements Iterable<Item>{
 
     class Node{
-	Item item;
-	Node next;
-	Node prev;
+	Item value;
+	Node next = null;
+	Node prev = null;
 
 	Node(Item item){
-	    item = item;
+	    value = item;
 	}
 
 	void setNext(Node item){
-	    
 	    next = item;
 	}
 	void setPrev(Node item){
 	    prev = item;
 	}
+	Item getItem(){
+	    return value;
+	}
+    }
 
+    private class DequeIterator implements Iterator<Item>{
+	Node current;
+	
+	public boolean hasNext(){
+	    if(current != null){
+	    	return true;		
+	    }
+	    return false;
+	}
+	
+	public Item next(){
+	    Node c = current;
+	    current = c.next;
+	    return c.getItem();
+	}
+
+	public void setCurrent(Node c){
+	    current = c;
+	}
     }
     
 
-    private Node first;
-    private Node last;
+    private Node first = null;
+    private Node last = null;
     private int n  = 0;
     
-    public Deque(){
-
-    }
 
     public boolean isEmpty(){
 	if(first == null && last == null){
@@ -44,45 +63,71 @@ public class Deque<Item> {
     }
 
     public void addFirst(Item item){
-	if(first == null && last == null){
+	if(first == null){
 	    Node node = new Node(item);
 	    first = node;
 
+	    if(last == null){
+		last = node;
+	    }
 	    n += 1;
 	    return;
 	}
 	Node new_first = new Node(item);
 	Node old_first = first;
 	new_first.setNext(old_first);
+	old_first.setPrev(new_first);
 	first = new_first;
-	
+	n += 1;
     }
     public void addLast(Item item){
+	if(last == null){
+	    Node node = new Node(item);
+	    last = node;
+	    
+	    if(first == null){
+		first = node;
+	    }
+	    n += 1;
+	    return;
+	}
+	
+	Node new_last = new Node(item);
+	Node old_last = last;
+	old_last.setNext(new_last);
+	new_last.setPrev(old_last);
+	last = new_last;
+	n += 1;
+    }
+    public Iterator<Item> iterator(){
+    	DequeIterator di = new DequeIterator();
+	di.setCurrent( first );
+    	return di;
+    }
+
+
+
+    public static void main(String [] args){
+
+	Deque<Integer> q = new Deque<Integer>();
+	q.addLast(1);
+	q.addLast(2);
+	q.addLast(3);
+	q.addLast(4);
+
+
+	Iterator x = q.iterator();
+
+	for(int i : q){
+	    System.out.println( i);
+	}
 
     }
-    // public Iterator<Item> iterator(){
-    // 	DequeIterator di = new DequeIterator();
-    // 	return di;
-    // }
-    
 
 }
 
 
 
-// class DequeIterator implements Iterator<Item>{
 
 
-//     public Item next(){
-	
-//     }
-    
-//     public boolean hasNext(){
-// 	return true;
-//     }
-//     public Item next(){
-// 	return null;
-//     }
-
-// }
 
