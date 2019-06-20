4fb89bc65aa203bcbfd81c7e01f0b4fbc187c68c
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2019-06-21 00:26:28 +0200

    startet på co-llineærtingen

--- p1a3/BruteCollinearPoints.java
+++ p1a3/BruteCollinearPoints.java
@@ -1,6 +1,43 @@
 
 public class BruteCollinearPoints {
-   public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
-   public           int numberOfSegments()        // the number of line segments
-   public LineSegment[] segments()                // the line segments
+
+
+    private Point points [];
+    private boolean colinear = true;
+    
+    public BruteCollinearPoints(Point[] points){
+	points = points;
+
+	Point first = points[0];
+	double s_compare = 0;
+	for(int i = 1; i < points.length; i+=1){
+	    
+	    double s = first.slopeTo( points[i] );
+	    if(s_compare == 0){
+		s_compare = s;
+	    }
+	    else if(s_compare != s){
+		colinear = false;
+	    }
+	}
+	System.out.println(colinear);
+    }// finds all line segments containing 4 points
+    public           int numberOfSegments(){
+	return 0;
+    }// the number of line segments
+    public LineSegment[] segments(){
+	return null;
+    }// the line segments
+
+
+    public static void main(String [] args){
+
+	Point [] ps = new Point[8];
+	
+	for(int i = 0; i < 8; i ++){
+	    ps[i] = new Point(i,i+2);
+	}
+	BruteCollinearPoints b = new BruteCollinearPoints(ps);
+	
+    }
 }
