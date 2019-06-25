f1e13f6d90ad3d3b6d9400844054429c141938fa
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2019-06-25 23:28:25 +0200

    fått 50 % riktig nå

--- p1a3/BruteCollinearPoints.java
+++ p1a3/BruteCollinearPoints.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdIn;
+import java.util.LinkedList;
 
 public class BruteCollinearPoints {
 
@@ -7,40 +8,44 @@ public class BruteCollinearPoints {
     private Point points [];
     
     private int n_segments = 0;
-    private LineSegment segments [];
+
+    
+    private LinkedList<LineSegment> segments;
     
     public BruteCollinearPoints(Point[] points){
 	points = points;
-
-	for(int i = 0; i < points.length; i++){
-	    Point first = points[i];
-	    double s_compare = 0;
-	    int i_compare = 1;
-	    boolean colinear = true;
-	    
-	    for(int y = i+1; y < points.length; y ++){
-		double s = first.slopeTo( points[y] );
-		if(s_compare == 0){
-		    s_compare = s;
-		    i_compare ++;
-		}
-		else if(s_compare != s){
-		    colinear = false;
-		}
+	segments = new LinkedList<LineSegment>();
+	
+	System.out.println(""+points.length);
+	for(int a = 0; a < points.length; a ++){
+	    for(int b = a+1; b < points.length; b ++){
+		double segAB = points[a].slopeTo( points[b] );
 		
-		if(i_compare == 4 && colinear){
-		    System.out.println("bigno");
+		for(int c = b+1; c < points.length; c++){
+		    double segBC = points[b].slopeTo( points[c]);
+		    
+		    for(int d = c+1; d < points.length; d++){
+			double segCD = points[c].slopeTo( points[d]);
+
+			if(segAB == segBC && segAB == segCD && segBC == segCD){
+			    n_segments ++;
+			    LineSegment sg = new LineSegment(points[a],points[d]);
+			    segments.add(sg);
+			}
+			
+		    }
 		}
 	    }
-	    System.out.println(i_compare);
 	}
-	System.out.println("Constructor finished");
-    }// finds all line segments containing 4 points
-    public           int numberOfSegments(){
-	return 0;
-    }// the number of line segments
+
+    }
+
+    public int numberOfSegments(){
+	return segments.size();
+    }
     public LineSegment[] segments(){
-	return null;
+	LineSegment [] s = new LineSegment[segments.size()];
+	return segments.toArray(s);
     }// the line segments
 
 
@@ -50,14 +55,16 @@ public class BruteCollinearPoints {
 	Point [] ps = new Point[size];
 
 	int i = 0;
-	while(! StdIn.isEmpty()){
+	while(!StdIn.isEmpty()){
 
 	    int x = StdIn.readInt();
 	    int y = StdIn.readInt();
 	    ps[i] = new Point(x,y);
 	    i ++;
+
 	}
 	BruteCollinearPoints b = new BruteCollinearPoints(ps);
-	
+
+	System.out.println(b.numberOfSegments());
     }
 }
--- p1a3/FastCollinearPoints.java
+++ p1a3/FastCollinearPoints.java
@@ -1,6 +1,17 @@
 
 public class FastCollinearPoints {
-   public FastCollinearPoints(Point[] points)     // finds all line segments containing 4 or more points
-   public           int numberOfSegments()        // the number of line segments
-   public LineSegment[] segments()                // the line segments
+    public FastCollinearPoints(Point[] points){
+
+    }// finds all line segments containing 4 or more points
+    public           int numberOfSegments(){
+	return 0;
+    }// the number of line segments
+    public LineSegment[] segments(){
+	return new LineSegment[1];
+    }// the line segments
+
+
+
+
+    
 }
