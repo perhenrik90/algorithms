ed04efc1807f3948107dd237a1b451b140df89f5
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2019-06-25 22:24:09 +0200

    added test, trying something else

--- p1a3/BruteCollinearPoints.java
+++ p1a3/BruteCollinearPoints.java
@@ -1,26 +1,40 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
 
 public class BruteCollinearPoints {
 
 
     private Point points [];
-    private boolean colinear = true;
+    
+    private int n_segments = 0;
+    private LineSegment segments [];
     
     public BruteCollinearPoints(Point[] points){
 	points = points;
 
-	Point first = points[0];
-	double s_compare = 0;
-	for(int i = 1; i < points.length; i+=1){
+	for(int i = 0; i < points.length; i++){
+	    Point first = points[i];
+	    double s_compare = 0;
+	    int i_compare = 1;
+	    boolean colinear = true;
 	    
-	    double s = first.slopeTo( points[i] );
-	    if(s_compare == 0){
-		s_compare = s;
+	    for(int y = i+1; y < points.length; y ++){
+		double s = first.slopeTo( points[y] );
+		if(s_compare == 0){
+		    s_compare = s;
+		    i_compare ++;
+		}
+		else if(s_compare != s){
+		    colinear = false;
+		}
+		
+		if(i_compare == 4 && colinear){
+		    System.out.println("bigno");
+		}
 	    }
-	    else if(s_compare != s){
-		colinear = false;
-	    }
+	    System.out.println(i_compare);
 	}
-	System.out.println(colinear);
+	System.out.println("Constructor finished");
     }// finds all line segments containing 4 points
     public           int numberOfSegments(){
 	return 0;
@@ -32,10 +46,16 @@ public class BruteCollinearPoints {
 
     public static void main(String [] args){
 
-	Point [] ps = new Point[8];
-	
-	for(int i = 0; i < 8; i ++){
-	    ps[i] = new Point(i,i+2);
+	int size = StdIn.readInt();
+	Point [] ps = new Point[size];
+
+	int i = 0;
+	while(! StdIn.isEmpty()){
+
+	    int x = StdIn.readInt();
+	    int y = StdIn.readInt();
+	    ps[i] = new Point(x,y);
+	    i ++;
 	}
 	BruteCollinearPoints b = new BruteCollinearPoints(ps);
 	
