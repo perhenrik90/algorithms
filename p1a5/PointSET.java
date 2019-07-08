733d1e68d5009ef206a2059e200af402788ace0e
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2019-07-08 17:41:07 +0200

    klart gjort til kode som kompilerer

--- p1a5/PointSET.java
+++ p1a5/PointSET.java
@@ -1,17 +1,37 @@
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 
 public class PointSET {
 
-    private SET<Point2D>
-   public         PointSET()                               // construct an empty set of points 
-   public           boolean isEmpty()                      // is the set empty? 
-   public               int size()                         // number of points in the set 
-   public              void insert(Point2D p)              // add the point to the set (if it is not already in the set)
-   public           boolean contains(Point2D p)            // does the set contain point p? 
-   public              void draw()                         // draw all points to standard draw 
-   public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle (or on the boundary) 
-   public           Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty 
+    private SET<Point2D> set;
+    
+    public         PointSET(){
 
-   public static void main(String[] args)                  // unit testing of the methods (optional) 
+    }// construct an empty set of points 
+    public           boolean isEmpty(){
+	return false;
+    }// is the set empty? 
+    public               int size(){
+	return 1;
+    }
+    public void insert(Point2D p){
+	
+    }
+    public boolean contains(Point2D p){
+	return false;
+    }
+    public void draw(){
+	
+    }
+    public Iterable<Point2D> range(RectHV rect){
+	return null;
+    }// all points that are inside the rectangle (or on the boundary) 
+    public Point2D nearest(Point2D p){
+	return null;
+    }// a nearest neighbor in the set to point p; null if the set is empty 
+
+    public static void main(String[] args){
+	System.out.println("Testing");
+    }// unit testing of the methods (optional) 
 }
